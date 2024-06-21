b88df0f15ba18803801b18d86a3a1814b45713bc
anshul <anshulagr799@gmail.com>
2024-06-21 16:07:54 +0530

    server

---
+++ Backend/model/blackList.js
@@ -0,0 +1,14 @@
+const mongoose = require( "mongoose" )  ;
+
+const BListSchema = mongoose.Schema(
+    {
+    token : { type : String, required : true }
+    },
+    {
+    versionKey : false
+    }
+)  ;
+
+const BListModel = mongoose.model( "Blacklist" , BListSchema )  ;
+
+module.exports={ BListModel }  ;
